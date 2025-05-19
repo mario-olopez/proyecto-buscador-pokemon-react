3c413a647e6b17a4e7e7297bb4ff2c6f8884b428
mario-olopez <mariolopezclav@gmail.com>
2025-05-19 20:12:04 +0200

    create/estructura básica proyecto

---
+++ buscador-pokemon/src/App.jsx
@@ -0,0 +1,18 @@
+import Head from "./components/Head"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Head />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
